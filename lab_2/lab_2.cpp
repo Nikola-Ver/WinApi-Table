fc535525df435e6c35e88af2299a8e0c1f452ea2
Nikola-Ver <46997500+Nikola-Ver@users.noreply.github.com>
2020-10-03 23:18:02 +0300

    new function

--- lab_2/lab_2.cpp
+++ lab_2/lab_2.cpp
@@ -2,28 +2,74 @@
 #include <fstream>
 #include "lab_2.h"
 
-std::string *list = NULL;
+//std::string* list = NULL;
+std::string list[3][3] = { 
+                            { {"text one one one one one"}, {"text two two two two two"}, {"text three three three three"} }, 
+                            { {"text three three three three"}, {"text one one one one one"}, {"text two two two two two"} },
+                            { {"text one one one one one"}, {"text three three three three"}, {"text two two two two two"} }
+                          };
+int  lenRow = 3, lenCol = 3;
+RECT rcSize;
+
+//VOID ReadFile()
+//{
+//    std::string line;
+//    std::ifstream file;
+//    file.open("table.txt");
+//
+//    while (file >> line) lenList++;
+//    list = new std::string[lenList];
+//    file.seekg(0, file.beg);
+//    for (int i = 0; i < lenList; i++) file >> list[i];
+//
+//    file.close();
+//}
+
+bool DrawLine(HDC hdc, POINT ptFrom, POINT ptTo)
+{
+    POINT pt;
+    int   x = ptFrom.x, 
+          y = ptFrom.y;
+    MoveToEx(hdc, x, y, &pt);
+    return LineTo(hdc, ptTo.x, ptTo.y);
+}
+
+VOID DrawTable(HDC hdc, RECT rcHwnd, std::string **listOfText[100], int _lenRow, int _lenCol)
+{
+    int sizeCol = (int)((rcSize.right - rcSize.left) / _lenCol);
+}
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+    HDC          hdc;
+    PAINTSTRUCT  ps;
+
     switch (msg)
     {
-        case WM_CREATE:
-        {
-            std::string line;
-            std::ofstream file;
-            file.open("./table.txt");
-            return 0;
-        }
-        case WM_DESTROY:
-        {
-            PostQuitMessage(0);
-            return 0;
-        }
-         default:
-        {
-            return DefWindowProc(hWnd, msg, wParam, lParam);
-        }
+    case WM_SIZE: 
+    {
+        GetClientRect(hWnd, &rcSize);
+        return 0;
+    }
+    case WM_CREATE:
+    {
+        return 0;
+    }
+    case WM_DESTROY:
+    {
+        //delete[](list);
+        PostQuitMessage(0);
+        return 0;
+    }
+    case WM_PAINT:
+        hdc = BeginPaint(hWnd, &ps);
+        DrawTable(hdc, rcSize, list, lenRow, lenCol);
+        EndPaint(hWnd, &ps);
+        return 0;
+    default:
+    {
+        return DefWindowProc(hWnd, msg, wParam, lParam);
+    }
     }
 }
 
@@ -72,7 +118,6 @@ int WINAPI WinMain(HINSTANCE hPrevInstance, HINSTANCE hInstance, LPSTR lpCmdLine
                 DispatchMessage(&msg);
             }
         }
-
     }
 
     return msg.wParam;
